3538baab3f02e2413fcf9356e840fed85c27fcb8
nasa <appsnasa57@gmail.com>
2023-11-24 00:33:26 -0800

    new project init

---
+++ security_in_spring/src/main/java/com/spring_security/security_in_spring/SecurityInSpringApplication.java
@@ -0,0 +1,13 @@
+package com.spring_security.security_in_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityInSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityInSpringApplication.class, args);
+	}
+
+}
---
+++ security_in_spring/src/test/java/com/spring_security/security_in_spring/SecurityInSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring_security.security_in_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityInSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
